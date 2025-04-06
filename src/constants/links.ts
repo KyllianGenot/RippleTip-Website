d5d232a726119d9bf5d1c25fa9249d57bfc69e55
KyllianGenot <kygenot@gmail.com>
2025-04-06 09:05:03 +0200

    feat: ✨ update discord link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,2 +1,2 @@
-export const BOT_INVITE_LINK: string = 'https://discord.com/oauth2/authorize?client_id=VOTRE_CLIENT_ID&permissions=VOS_PERMISSIONS&scope=bot%20applications.commands';
+export const BOT_INVITE_LINK: string = 'https://discord.com/oauth2/authorize?client_id=1344826332332298260&permissions=2147567616&integration_type=0&scope=bot+applications.commands';
 export const GITHUB_REPO_LINK: string = 'https://github.com/RippleTip';
